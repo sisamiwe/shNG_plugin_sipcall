4caf0d49c4995929462f0ff2a2d83a9dd06b1b38
sisamiwe <wenzel_michael@web.de>
2023-01-26 16:01:31 +0100

    BugFix

--- webif/__init__.py
+++ webif/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # vim: set encoding=utf-8 tabstop=4 softtabstop=4 shiftwidth=4 expandtab
 #########################################################################
-#  Copyright 2020-     <AUTHOR>                                   <EMAIL>
+#  Copyright 2023 -      Michael Wenzel             wenzel_michael@web.de
 #########################################################################
 #  This file is part of SmartHomeNG.
 #  https://www.smarthomeNG.de
@@ -61,7 +61,6 @@ class WebInterface(SmartPluginWebIf):
 
         self.tplenv = self.init_template_environment()
 
-
     @cherrypy.expose
     def index(self, reload=None):
         """
@@ -80,15 +79,40 @@ class WebInterface(SmartPluginWebIf):
         # try to get the webif pagelength from the plugin specific plugin.yaml configuration
         try:
             pagelength = self.plugin.webif_pagelength
-            self.logger.debug("Plugin pagelength {}".format(pagelength))
+            self.logger.debug(f"Plugin pagelength {pagelength}")
         except Exception:
             pass
         # add values to be passed to the Jinja2 template eg: tmpl.render(p=self.plugin, interface=interface, ...)
         return tmpl.render(p=self.plugin,
                            webif_pagelength=pagelength,
-                           items=sorted(self.items.return_items(), key=lambda k: str.lower(k['_path'])),
-                           item_count=0)
+                           items=self.plugin._itemlist,
+                           item_count=len(self.plugin._itemlist))
 
+    @cherrypy.expose
+    def get_data_html(self, dataSet=None):
+        """
+        Return data to update the webpage
+
+        For the standard update mechanism of the web interface, the dataSet to return the data for is None
+
+        :param dataSet: Dataset for which the data should be returned (standard: None)
+        :return: dict with the data needed to update the web page.
+        """
+        if dataSet is None:
+            data = {}
+            data['item'] = {}
+
+            for item in self.plugin._itemlist:
+                data['item'][item]['value'] = item()
+                self.logger.debug(f"Plugin item {item}")
+
+            # return it as json to the web page
+            self.logger.debug(f"Plugin data {data}")
+
+            try:
+                return json.dumps(data, default=str)
+            except Exception as e:
+                self.logger.error(f"get_data_html exception: {e}")
 
     @cherrypy.expose
     def get_data_html(self, dataSet=None):
@@ -101,6 +125,9 @@ class WebInterface(SmartPluginWebIf):
         :return: dict with the data needed to update the web page.
         """
         # if dataSets are used, define them here
+
+        self.logger.debug(f"get_data_html called for dataSet={dataSet}")
+
         if dataSet == 'overview':
             # get the new data from the plugin variable called _webdata
             data = self.plugin._webdata
@@ -109,17 +136,23 @@ class WebInterface(SmartPluginWebIf):
                 return data
             except Exception as e:
                 self.logger.error(f"get_data_html exception: {e}")
-        if dataSet is None:
-            # get the new data
-            data = {}
 
-            # data['item'] = {}
-            # for i in self.plugin.items:
-            #     data['item'][i]['value'] = self.plugin.getitemvalue(i)
-            #
-            # return it as json the the web page
-            # try:
-            #     return json.dumps(data)
-            # except Exception as e:
-            #     self.logger.error("get_data_html exception: {}".format(e))
+        else:
+            self.logger.debug(f"Create WebIF Update data for {dataSet}")
+            # get the new data
+            item_info = {}
+            for item in self.plugin._itemlist:
+                item_info[item.id()] = {}
+                item_info[item.id()]['value'] = item()
+                item_info[item.id()]['last_update'] = item.property.last_update.strftime('%d.%m.%Y %H:%M:%S')
+                item_info[item.id()]['last_change'] = item.property.last_change.strftime('%d.%m.%Y %H:%M:%S')
+
+            self.logger.debug(f"WebIF Update data: {item_info}")
+
+
+            # return it as json to the web page
+            try:
+                return json.dumps(item_info)
+            except Exception as e:
+                self.logger.error("get_data_html exception: {}".format(e))
         return {}
